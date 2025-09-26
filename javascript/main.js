b333ac90bda6d3b00d42b2217c3ddd669973f365
Soy Daniel <danielfcp34@gmail.com>
2025-09-25 19:03:42 -0500

    botones agregar index

--- javascript/main.js
+++ javascript/main.js
@@ -1,69 +1,9 @@
-const productos = [
-  {
-    id: "Teclado01",
-    titulo: "Teclado01",
-    imagen: "img/teclado1.jpg",
-    categoria: {
-      nombre: "Teclados",
-      id: "teclado",
-    },
-    precio: 1000,
-  },
-  {
-    id: "Teclado02",
-    titulo: "Teclado02",
-    imagen: "img/teclado2.jpg",
-    categoria: {
-      nombre: "Teclados",
-      id: "teclado",
-    },
-    precio: 1500,
-  },
-  {
-    id: "Teclado03",
-    titulo: "Teclado03",
-    imagen: "img/teclado3.jpg",
-    categoria: {
-      nombre: "Teclados",
-      id: "teclado",
-    },
-    precio: 1000,
-  },
-  {
-    id: "Mouse01",
-    titulo: "Mouse01",
-    imagen: "img/mouse1.jpg",
-    categoria: {
-      nombre: "Mouse",
-      id: "mouse",
-    },
-    precio: 1000,
-  },
-  {
-    id: "Mouse02",
-    titulo: "Mouse02",
-    imagen: "img/mouse2.jpg",
-    categoria: {
-      nombre: "Mouse",
-      id: "mouse",
-    },
-    precio: 1000,
-  },
-  {
-    id: "Mouse03",
-    titulo: "Mouse03",
-    imagen: "img/mouse3.jpg",
-    categoria: {
-      nombre: "Mouse",
-      id: "mouse",
-    },
-    precio: 1000,
-  },
-];
 // DOM
 const contenedorProductos = document.querySelector(".contenedor_productos");
 const botonesCategoria = document.querySelectorAll(".boton_categoria");
 const TituloPrincipal = document.querySelector(".titulo_principal");
+let botonesAgregar = document.querySelectorAll(".producto_agregar");
+const numerito = document.querySelector(".numerito");
 
 function CargarProductos(productosElegidos) {
   contenedorProductos.innerHTML = " ";
@@ -79,6 +19,8 @@ function CargarProductos(productosElegidos) {
               `;
     contenedorProductos.append(div);
   });
+  actualizarBotonesAgregar();
+  console.log(botonesAgregar);
 }
 CargarProductos(productos);
 
@@ -101,3 +43,38 @@ botonesCategoria.forEach((boton) => {
     }
   });
 });
+function actualizarBotonesAgregar() {
+  botonesAgregar = document.querySelectorAll(".producto_agregar");
+  botonesAgregar.forEach((boton) => {
+    boton.addEventListener("click", agregarAlCarrito);
+  });
+}
+
+const productosEnCarrito = [];
+
+function agregarAlCarrito(e) {
+  const idBoton = e.currentTarget.id;
+  const productoAgregado = productos.find(
+    (producto) => producto.id === idBoton
+  );
+  if (productosEnCarrito.some((producto) => producto.id === idBoton)) {
+    const index = productosEnCarrito.findIndex(
+      (producto) => producto.id === idBoton
+    );
+    productosEnCarrito[index].cantidad++;
+  } else {
+    productosEnCarrito.push({
+      ...productoAgregado,
+      cantidad: 1,
+    });
+  }
+  actualizarNumerito();
+}
+
+function actualizarNumerito() {
+  let nuevoNumerito = productosEnCarrito.reduce(
+    (acc, producto) => acc + producto.cantidad,
+    0
+  );
+  numerito.innerText = nuevoNumerito;
+}
---
+++ javascript/productos.js
@@ -0,0 +1,62 @@
+const productos = [
+  {
+    id: "Teclado01",
+    titulo: "Teclado01",
+    imagen: "img/teclado1.jpg",
+    categoria: {
+      nombre: "Teclados",
+      id: "teclado",
+    },
+    precio: 1000,
+  },
+  {
+    id: "Teclado02",
+    titulo: "Teclado02",
+    imagen: "img/teclado2.jpg",
+    categoria: {
+      nombre: "Teclados",
+      id: "teclado",
+    },
+    precio: 1500,
+  },
+  {
+    id: "Teclado03",
+    titulo: "Teclado03",
+    imagen: "img/teclado3.jpg",
+    categoria: {
+      nombre: "Teclados",
+      id: "teclado",
+    },
+    precio: 1000,
+  },
+  {
+    id: "Mouse01",
+    titulo: "Mouse01",
+    imagen: "img/mouse1.jpg",
+    categoria: {
+      nombre: "Mouse",
+      id: "mouse",
+    },
+    precio: 1000,
+  },
+  {
+    id: "Mouse02",
+    titulo: "Mouse02",
+    imagen: "img/mouse2.jpg",
+    categoria: {
+      nombre: "Mouse",
+      id: "mouse",
+    },
+    precio: 1000,
+  },
+  {
+    id: "Mouse03",
+    titulo: "Mouse03",
+    imagen: "img/mouse3.jpg",
+    categoria: {
+      nombre: "Mouse",
+      id: "mouse",
+    },
+    precio: 1000,
+  },
+];
